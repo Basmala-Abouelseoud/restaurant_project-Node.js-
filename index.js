5daed10473002b2019e25b1b699d1bcacf65b60b
Basmala Abouelseoud <basmala.abouelseoud@gmail.com>
2026-05-18 10:11:19 +0300

    Fix Vercel configuration

--- index.js
+++ index.js
@@ -100,7 +100,10 @@ app.use(globalErrorHandler);
 
 // ─── Start server ─────────────────────────────────────────────────────────────
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-  console.log(`Swagger docs: http://localhost:${PORT}/api-docs`);
-});
+// app.listen(PORT, () => {
+//   console.log(`Server running on port ${PORT}`);
+//   console.log(`Swagger docs: http://localhost:${PORT}/api-docs`);
+// });
+
+
+module.exports = app;
